5bdc9957881512c6c41cc4c251d168da56e92c57
Don <dxanderr@gmail.com>
2022-03-31 23:36:49 +0200

    form added to register page - in progress

--- src/components/pages/Register.jsx
+++ src/components/pages/Register.jsx
@@ -3,7 +3,24 @@ import React from "react";
 export default function Register() {
     return (
         <div className="register-container">
+            <form>
+                <h4>Register</h4>
+                <p className="header-text">Sign up to book appointments</p>
+                <p className="form-text">Full Name</p>
+                <input type="text" className="input-field"></input>
+                <p className="form-text">Email</p>
+                <input type="text" className="input-field"></input>
+                <p className="form-text">Mobile Phone Number</p>
+                <input type="text" className="input-field"></input>
+                <p className="form-text">Password (6 character minimum)</p>
+                <input type="text" className="input-field"></input>
+                <p className="form-text">Password Confirmation</p>
+                <input type="text" className="input-field"></input>
+                <button>Sign up</button>
+            </form>
 
+            <a href="/">Log in</a><br></br>
+            <a href="/">Didn't receive confirmation instructions?</a>
         </div>
     );
 }
