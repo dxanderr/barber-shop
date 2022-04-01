e5b1c215a58be23e3d0c18f0e7a69a46a6880593
Don <dxanderr@gmail.com>
2022-04-01 22:23:22 +0200

    additions to login page - form field

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -3,7 +3,19 @@ import React from "react";
 export default function Login() {
     return (
         <div className="login-container">
+            <form>
+                <h4>Login</h4>
+                <p className="header-text">Login to book an appointment</p>
+                <p className="form-text">Email</p>
+                <input type="text" className="input-field"></input>
+                <p className="form-text">Password</p>
+                <input type="text" className="input-field"></input>
 
+                <button>Log in</button>
+            </form>
+
+            <a href="/register">Sign up</a><br></br>
+            <a href="/">Didn't receive confirmation instructions?</a>
         </div>
     );
 }
