b41fe38a3efd0a0190dc256d0aa9fe4e0eba0778
Don <dxanderr@gmail.com>
2022-04-02 17:27:00 +0200

    additions to gallery - grid

--- src/components/GalleryData.jsx
+++ src/components/GalleryData.jsx
@@ -2,27 +2,43 @@
 const Photos = [
     {
         id: 1,
-        image: "./images/gallery/gallery1.jpg"
+        image: "./images/gallery/gallery1.jpg",
+        title: "Lorem"
     },
     {
         id: 2,
-        image: "./images/gallery/gallery2.jpg"
+        image: "./images/gallery/gallery2.jpg",
+        title: "Ipsum"
     },
     {
         id: 3,
-        image: "./images/gallery/gallery3.jpg"
+        image: "./images/gallery/gallery3.jpg",
+        title: "Lorem"
     },
     {
         id: 4,
-        image: "./images/gallery/gallery1.jpg"
+        image: "./images/gallery/gallery1.jpg",
+        title: "Ipsum"
     },
     {
         id: 5,
-        image: "./images/gallery/gallery2.jpg"
+        image: "./images/gallery/gallery2.jpg",
+        title: "Lorem"
     },
     {
         id: 6,
-        image: "./images/gallery/gallery3.jpg"
+        image: "./images/gallery/gallery3.jpg",
+        title: "Ipsum"
+    },
+    {
+        id: 7,
+        image: "./images/gallery/gallery1.jpg",
+        title: "Ipsum"
+    },
+    {
+        id: 8,
+        image: "./images/gallery/gallery2.jpg",
+        title: "Ipsum"
     },
 ]
 
--- src/components/pages/Gallery.jsx
+++ src/components/pages/Gallery.jsx
@@ -1,27 +1,24 @@
 import React from "react";
-// import Photos from "../GalleryData";
+import Photos from "../GalleryData";
 
 export default function Gallery() {
-    // const galleryPhotos = Photos.map(photo =>{
-    //     return (
-    //         <a href="/" className="photo-link">
-    //             <div 
-    //                 key={photo.id} 
-    //                 alt="gallery" 
-    //                 className="photo"
-    //                 style={{
-    //                     gridArea:`img-${photo.id}`, 
-    //                     backgroundImage:`url(${photo.image})`
-    //                 }}
-    //             ></div>
-    //         </a>
-    //     )
-    // })
+    const galleryPhotos = Photos.map(photo =>{
+        return (
+            <div className="gallery-item-wrap">
+                <div className="gallery-item">
+                    <div className="image">
+                        <img src={photo.image} alt={photo.title}/>
+                    </div>
+                    <div className="gallery-text">{photo.title}</div>
+                </div>
+            </div>
+        )
+    })
     return (
         <div className="gallery-container">
             <h4>Gallery</h4>
-            <div className="photo-grid">
-                {/* {galleryPhotos}     */}
+            <div className="gallery-wrap">
+                {galleryPhotos}    
             </div>
         </div>
     );
